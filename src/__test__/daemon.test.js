1c34049c909c6a652a2102d9b88897211884c0ee
Nikita Shalimov <nikitashalimov@mail.ru>
2021-10-29 22:34:11 +0300

    теперь ошибок нет

--- src/__test__/daemon.test.js
+++ src/__test__/daemon.test.js
@@ -1,14 +1,14 @@
-import Daemon from "../daemon";
+import Daemon from '../daemon';
 
 test('проверка персонажа-демона', () => {
-	const unit1 = new Daemon('демон', 'Daemon');
-	const unit2 = {
-		name: 'демон',
-		type: 'Daemon',
-		health: 100,
-		level: 1,
-		attack: 10,
-		defence: 40,
-	};
-	expect(unit1).toEqual(unit2);
+  const unit1 = new Daemon('демон', 'Daemon');
+  const unit2 = {
+    name: 'демон',
+    type: 'Daemon',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  };
+  expect(unit1).toEqual(unit2);
 });
--- src/character.js
+++ src/character.js
@@ -1,19 +1,19 @@
 export default class Character {
-	constructor(name, type) {
-		//имя должно быть строкой от 2 до 10 символов
-		if (typeof name == 'string' && name.length >= 2 && name.length <= 10) {
-			this.name = name;
-		} else {
-			throw new Error("the character's name must contain letters from 2 to 10 characters")
-		}
-		//тип должен быть один из 6-ти предложенных
-		if (type == 'Boweman' || type == 'Swordsman' || type == 'Magician' || type == 'Daemon' || type == 'Undead' || type == 'Zombie') {
-			this.type = type;
-		} else {
-			throw new Error("Please select one of the available classes: Boweman, Swordsman, Magician, Daemon, Undead, Zombie")
-		}
-		this.type = type;
-		this.health = 100;
-		this.level = 1;
-	}
+  constructor(name, type) {
+    // имя должно быть строкой от 2 до 10 символов
+    if (typeof name === 'string' && name.length >= 2 && name.length <= 10) {
+      this.name = name;
+    } else {
+      throw new Error("the character's name must contain letters from 2 to 10 characters");
+    }
+    // тип должен быть один из 6-ти предложенных
+    if (type === 'Boweman' || type === 'Swordsman' || type === 'Magician' || type === 'Daemon' || type === 'Undead' || type === 'Zombie') {
+      this.type = type;
+    } else {
+      throw new Error('Please select one of the available classes: Boweman, Swordsman, Magician, Daemon, Undead, Zombie');
+    }
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+  }
 }
