1bf19d75a8d969cecb0e71ac3a1213273878bdf9
Nikita Shalimov <nikitashalimov@mail.ru>
2021-11-01 20:10:07 +0300

    после исправления замечаний

--- src/character.js
+++ src/character.js
@@ -1,4 +1,12 @@
 export default class Character {
+  static typeContainer = [
+    'Boweman',
+    'Swordsman',
+    'Magician',
+    'Daemon',
+    'Undead',
+    'Zombie'
+  ];
   constructor(name, type) {
     // имя должно быть строкой от 2 до 10 символов
     if (typeof name === 'string' && name.length >= 2 && name.length <= 10) {
@@ -7,12 +15,11 @@ export default class Character {
       throw new Error("the character's name must contain letters from 2 to 10 characters");
     }
     // тип должен быть один из 6-ти предложенных
-    if (type === 'Boweman' || type === 'Swordsman' || type === 'Magician' || type === 'Daemon' || type === 'Undead' || type === 'Zombie') {
+    if (typeContainer.includes(type)) {
       this.type = type;
     } else {
       throw new Error('Please select one of the available classes: Boweman, Swordsman, Magician, Daemon, Undead, Zombie');
     }
-    this.type = type;
     this.health = 100;
     this.level = 1;
   }
