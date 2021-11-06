ba74de674ff0447583134350aa6549d25c7fc9f3
Nikita Shalimov <nikitashalimov@mail.ru>
2021-11-06 17:07:04 +0300

    исправил

--- src/__test__/boweman.test.js
+++ src/__test__/boweman.test.js
@@ -12,3 +12,23 @@ test('проверка персонажа-лучника', () => {
   };
   expect(unit1).toEqual(unit2);
 });
+
+test('проверка по длине имени у персонажа лучника', () => {
+  const unit1 = () => new Boweman(1, 'Boweman');
+  expect(unit1).toThrow("the character's name must contain letters from 2 to 10 characters");
+});
+
+test('проверка по длине имени у персонажа лучника', () => {
+  const unit1 = () => new Boweman('Q', 'Boweman');
+  expect(unit1).toThrow("the character's name must contain letters from 2 to 10 characters");
+});
+
+test('проверка по длине имени у персонажа лучника', () => {
+  const unit1 = () => new Boweman('Luuuchniiikkkkk', 'Boweman');
+  expect(unit1).toThrow("the character's name must contain letters from 2 to 10 characters");
+});
+
+test('проверка класса персонажа лучника', () => {
+  const unit1 = () => new Boweman('unit', 'luchnik');
+  expect(unit1).toThrow('Please select one of the available classes: Boweman, Swordsman, Magician, Daemon, Undead, Zombie');
+});
